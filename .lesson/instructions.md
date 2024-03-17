c126b429240641011208e23a742ad401fd916b25
Maru Puran <144689560+mpur6520@users.noreply.github.com>
2024-03-17 19:51:49 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,13 @@
+# Task - Investigate
+
+Use comments to answer the investigate questions on the example code.
+
+## Help! My Code Doesn't Work!
+
+Make sure that you check for the following things:
+
+- you have put the brackets after the input command
+- you have typed the variable name **exactly** the same (including caps), every time
+- There is a variable on the **left** of each assignment
+- You have used a single **=** for assignment
+- You have used the **+** symbol to concatenate (join).
---
+++ main.py
@@ -0,0 +1,25 @@
+name = "Billy"
+print("We want to know if you like progamming!")
+print()
+print("Do you like programming " + name + "?")
+answer = input()
+print("Great, " + name + ", you said " + answer + "!")
+print("Let's learn some Python today")
+
+# How many output statements are used in the code?
+ # Answer: There are 5 output statements used in the code.
+
+# What symbol is used for concatenation?
+ # Answer: For concatenation, the symbol + is used.
+
+# What is the purpose of the program?
+ # Answer: It will ask Billy ("Billy" is the string in the name variable) whether or not they like computer programming, let him answer and then motivate Billy to learn some python today. 
+
+# What line(s) is assignment used on?
+ # Answer: It is used on lines 1 and 5.
+
+# What symbol is used for assignment?
+ # Answer: To assign a value, the symbol = is used.
+
+# What is the purpose of the empty print statement on line 3?
+ # Answer: It is to create an empty line to separate two statements; a line break w/ empty space.
